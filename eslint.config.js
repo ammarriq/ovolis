1166f653198f6073b4aafa13bd22f4399830273b
Ammar Iqbal <ammarriqbalg@gmail.com>
2025-08-16 13:35:33 +0500

    fix dev server

---
+++ eslint.config.js
@@ -0,0 +1,77 @@
+import { defineConfig } from "eslint/config"
+import pluginReact from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+    reactHooks.configs["recommended-latest"],
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+        },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+
+        rules: {
+            "react/jsx-uses-react": "off",
+            "react/react-in-jsx-scope": "off",
+            "no-undef": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/no-empty-object-type": [
+                "error",
+                { allowInterfaces: "always" },
+            ],
+            "@typescript-eslint/consistent-type-imports": "error",
+            "react/prop-types": ["error", { ignore: ["className"] }],
+
+            "sort-imports": "off",
+            "import/order": "off",
+            "simple-import-sort/imports": [
+                "error",
+                {
+                    groups: [
+                        ["^\\u0000"],
+                        ["^.+\\u0000$"],
+                        [
+                            "^node:",
+                            "electron",
+                            "^react",
+                            "react-aria-components",
+                        ],
+                        ["^@?\\w"],
+                        ["^~/lib", "^"],
+                        ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+                        ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+                    ],
+                },
+            ],
+        },
+    },
+])
---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.body)
+root.render(
+    <div className="bg-red-500">
+        <h2>Hello from React!</h2>
+    </div>
+)
